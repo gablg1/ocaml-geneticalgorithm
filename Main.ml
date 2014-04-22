3b52e017560de94aea41fb82342e6e9b06aea752
gablg1 <gabrielg@Gabriels-MacBook-Pro.local>
2014-04-22 00:11:15 -0400

    Done debugging

--- Main.ml
+++ Main.ml
@@ -9,15 +9,4 @@ let _ = Random.init (Float.to_int (Unix.time ())) in
 
 Polygon.run_tests ();;
 
-print_endline (Float.to_string (Statistics.gaussian_pick 3. 1.));;
-print_endline (Float.to_string (Statistics.gaussian_pick 3. 1.));;
-print_endline (Float.to_string (Statistics.gaussian_pick 3. 1.));;
-print_endline (Float.to_string (Statistics.gaussian_pick 3. 1.));;
-print_endline (Float.to_string (Statistics.gaussian_pick 3. 1.));;
-print_endline (Float.to_string (Statistics.gaussian_pick 3. 1.));;
-print_endline (Float.to_string (Statistics.gaussian_pick 3. 1.));;
-print_endline (Float.to_string (Statistics.gaussian_pick 3. 1.));;
-print_endline (Float.to_string (Statistics.gaussian_pick 3. 1.));;
-print_endline (Float.to_string (Statistics.gaussian_pick 3. 10.));;
-print_endline (Float.to_string (Statistics.gaussian_pick 3. 100.));;
 
