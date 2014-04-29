9a10e9995cc8f20b1b1647b9c34944dcb8220f51
Sharon Zhou <Zhou12@college.harvard.edu>
2014-04-29 16:45:49 -0400

    file saveworks

--- Display.ml
+++ Display.ml
@@ -30,17 +30,22 @@ draw_from_colors m 4
 
 
 let save_img_file matrix i =
-  let file = "iteration[" ^ (string_of_int i) ^ "].ml" in
+  let file = "iteration-" ^ (string_of_int i) ^ ".ml" in
+  let num_message = (string_of_int i) ^ "\n" in
   let message = 
-    (string_of_int i) ^ "/n"
-    for i = 0 to (Array.length matrix) do
-    (Array.fold_right ~f(fun x y -> (string_of_int x) ^ " " ^ y) ~init:"") ^ "/n"
-    done 
+    let f = Array.fold_right ~f:(fun x y -> (string_of_int x) ^ " " ^ y) ~init:"" in
+    Array.fold_right matrix ~init:"" ~f:(fun x y -> (f x) ^ "\n" ^ y)
   in
   let oc = open_out file in   
-  fprintf oc "%s\n" message; close_out_noerr oc
-;;              
-  
+  fprintf oc "%s\n%s" num_message message; close_out_noerr oc
+;;   
+
+(* testing *)
+let color1 = Graphics.rgb 100 150 300 in
+let m = Array.make_matrix 200 300 color1 in           
+save_img_file m 4
+;;  
+
 let show_img_file file =
   let ic = open_in file in
   let line = input_line ic in  
@@ -48,6 +53,10 @@ let show_img_file file =
   (* let i = first line, then skip the /n, then make a matrix named m
 let m = (make from matrices?), and finally: 
 Graphics.open_graph " 200x400"; draw_from_colors m i*)
-;;              
+;;       
+
+(* testing *)
+show_img_file "iteration-4.ml"
+;;         
 
 
