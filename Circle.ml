ba0d6a1c45d6de2ccf675600590dd14cd2028ab1
gablg1 <gabrielg@Gabriels-MacBook-Pro.local>
2014-04-30 00:19:33 -0400

    Using circles

---
+++ Circle.ml
@@ -0,0 +1,149 @@
+open Core.Std
+open Graphics
+open Statistics
+open Helpers.Helpers
+
+(* Module that performs operations on Circles *)
+module type CIRCLE =
+sig
+  type circle
+  
+  (* 'fresh a b v' returns a fresh randomly initialized circle 
+   * with center within the rectangle delimited by (0,0) (a, b) *)
+  val fresh : int -> int -> circle
+  
+  (* Returns the center of the circle *)
+  val center : circle -> (float * float)
+  
+  (* Returns the radius of the circle *)
+  val radius : circle -> float
+  
+  (* Makes a new circle out of a center, a radius, and a color *)
+  val make : (float * float) -> float -> color -> circle
+  
+  (* Returns the color of the circle as an RGB triple *)
+  val rgb : circle -> (int * int * int)
+  
+  (* Returns the color of the circle *)
+  val color : circle -> color
+  
+  (* Returns whether circle contains point *)
+  val contains : circle -> (float * float) -> bool
+  
+  (* 'sexual_reproduction p1 p2 c' returns a circle
+   * daughter of p1 and p2 with crossing over level of c *)
+  val sexual_reproduction : float -> circle -> circle -> circle
+  
+  (* Returns true if two circles are equal and false otherwise *)
+  val equals : circle -> circle -> bool
+  
+  (* Prints a circle *)
+  val print : circle -> unit
+  
+  (* Runs tests on this Module *)
+  val run_tests : unit -> unit
+end
+
+module Circle : CIRCLE with type circle= (float * float) * float * color =
+struct
+  
+  type circle = (float * float) * float * color
+
+  let fresh a b = 
+    let fa, fb = Float.of_int a, Float.of_int b in
+    (random_point fa fb, Random.float (max fa fb), random_color ())
+
+  let make (ctr : (float * float)) (r : float) (c : color) = (ctr,r,c)
+  
+  let center (ctr,_,_) = ctr
+  
+  let center_x circ = 
+    let x,_ = center circ in 
+    x
+    
+  let center_y circ = 
+    let _,y = center circ in 
+    y
+
+  let radius (_,r,_) = r
+
+  let color (_,_,col) = col
+
+  let rgb circ = to_rgb (color circ)
+
+  let point_reproduction std_dev p1 p2 =
+    let (x,y) = halfway_point p1 p2 in
+    (Statistics.gaussian_pick std_dev x, Statistics.gaussian_pick std_dev y)
+
+  let color_reproduction std_dev c1 c2 =
+    let f a1 a2 = Int.of_float (Statistics.gaussian_pick std_dev ((Float.of_int (a1 + a2)) /. 2.0)) in
+    let (r1,g1,b1) = to_rgb c1
+    and (r2,g2,b2) = to_rgb c2 in
+    Graphics.rgb (f r1 r2) (f g1 g2) (f b1 b2)
+    
+  let float_reproduction std_dev r1 r2 =
+    let mid_point = (r1 +. r2) /. 2. in
+    Statistics.gaussian_pick std_dev mid_point  
+  
+  let sexual_reproduction std_dev circ1 circ2 = 
+    let new_center = point_reproduction std_dev (center circ1) (center circ2) in
+    let new_radius = float_reproduction std_dev (radius circ1) (radius circ2) in
+    let new_color = color_reproduction std_dev (color circ1) (color circ2) in
+    (new_center, new_radius, new_color)
+
+  let test_center () =
+    let p1 = make (0.,0.) 3. blue in
+    let p2 = make (1.,2.) 4. red in
+    let p3 = make (3.,4.) 5. green in
+    assert(center p1 = (0., 0.));
+    assert(center p2 = (1.,2.));
+    assert(center p3 = (3.,4.))
+
+  let test_color () =
+    let p1 = make (0.,0.) 3. blue in
+    let p2 = make (1.,2.) 4. red in
+    let p3 = make (3.,4.) 5. green in
+    assert(color p1 = blue);
+    let r,g,b = rgb p1 in
+    assert(Graphics.rgb r g b = blue);
+    assert(color p2 = red);
+    let r,g,b = rgb p2 in
+    assert(Graphics.rgb r g b = red);
+    assert(color p3 = green);
+    let r,g,b = rgb p3 in
+    assert(Graphics.rgb r g b = green)
+
+  let contains circ (x,y) =
+    let x', y' = center_x circ, center_y circ in
+    (radius circ)**2. >= (x' -. x)**2. +. (y' -. y)**2.
+
+  (* Prints a circle for testing purposes *)
+  let print p =
+    print_endline "######### Start of Circle #########";
+    Printf.printf "Center: (%f, %f)\nRadius: %f\nColor: %x\n" (center_x p) (center_y p) (radius p) (color p);
+    print_endline "######### End of Circle #########"
+
+  let equals p1 p2 =
+    center p1 = center p2 && radius p1 = radius p2 && color p1 = color p2
+
+  let test_sexual_reproduction () =
+    let p1 = make (0.,0.) 3. blue in
+    let p2 = make (1.,2.) 4. red in
+    let p3 = make (3.,4.) 5. green in
+    
+    let r1 = make (0.5,1.) 3.5 (halfway_color blue red) in
+    let r2 = make (2.,3.) 4.5 (halfway_color red green) in
+    let r3 = make (1.5,2.) 4. (halfway_color blue green) in
+    
+    assert(equals (sexual_reproduction 0. p1 p2) r1);
+    assert(equals (sexual_reproduction 0. p2 p3) r2);
+    assert(equals (sexual_reproduction 0. p1 p3) r3)
+
+  let run_tests () =
+    test_center ();
+    test_color ();
+    test_sexual_reproduction ();
+    ()
+
+end
+
--- Guess.ml
+++ Guess.ml
@@ -2,32 +2,34 @@ open Core.Std
 open Graphics
 open Helpers.Helpers
 open Polygon
+open Circle
 
 module type GUESS =
 sig
-  type polygon
+  (* lego is the building block of the image *)
+  type lego
 
   type guess
 
-  (* fresh w h m v returns a new random guess with m polygons and v vertices 
+  (* fresh w h m returns a new random guess with m legos 
    * where w and h are the width and height of the target image *)
-  val fresh : int -> int -> int -> int -> guess
+  val fresh : int -> int -> int -> guess
 
-  (* Returns list of polygons that composes guess *)
-  val polygons : guess -> polygon list
+  (* Returns list of legos that composes guess *)
+  val legos : guess -> lego array
 
-  (* Returns an image of the guess *)
-  val image_of_guess : guess -> image
+  (* Returns guess as color array array *)
+  val matrix_of_guess : guess -> color array array
   
   (* Measures the fitness of the guess against the target.
    * The greater the weight (between 0 and MAX_COST) the better. *)
   val cost : guess -> image -> float
 
   (* Makes a new guess out of a polygon list *)
-  val make : polygon list -> guess
+  val make : lego list -> guess
 
   (* Performs sexual reproduction between this guess and another guess
-   * by calling reproduction on each of the correspondent Ps.
+   * by calling reproduction on each of the correspondent legos.
    * Third argument is the level of chaos of the reproduction
    * Returns a daughter guess *)
   val sexual_reproduction : float -> guess -> guess -> guess
@@ -42,18 +44,76 @@ sig
   val run_tests : unit -> unit
 end
 
-module MakeGuess (P : POLYGON) : GUESS with type guess=P.polygon array =
+module MakeCircleGuess (C : CIRCLE) : GUESS with type guess=C.circle array =
 struct
-  type polygon = P.polygon
-  type guess = polygon array
+  type lego = C.circle
+  type guess = lego array
   
-  let fresh width height m v = Array.init ~f:(fun _ -> P.fresh width height v) m
+  let fresh width height m = Array.init ~f:(fun _ -> C.fresh width height) m
   
-  let polygons g = Array.to_list g
+  let legos g = g
 
   let make lst = Array.of_list lst
 
-  let image_of_guess _ = Graphics.make_image (Array.make_matrix ~dimx:5 ~dimy:12 black)
+  (* Placeholder *)
+  let matrix_of_guess _ = Array.make_matrix ~dimx:5 ~dimy:12 black
+
+  let cost _ _ = failwith "TODO"
+
+  let sexual_reproduction std_dev g1 g2 = Array.map2_exn ~f:(C.sexual_reproduction std_dev) g1 g2
+  
+  let equals g1 g2 = Array.fold2_exn ~f:(fun r p1 p2 -> (C.equals p1 p2) && r) ~init:(true) g1 g2
+  
+  let print g =
+    print_endline "################ Start of Guess ################";
+    Array.iter ~f:(C.print) g;
+    print_endline "################ End of Guess ################";
+    print_endline ""
+  
+  let test_sexual_reproduction () =
+    let p1 = C.make (0.,0.) 3. blue in
+    let p2 = C.make (1.,2.) 4. red in
+    let p3 = C.make (3.,4.) 5. green in
+    
+    let g1 = make [p1] in
+    let g2 = make [p2] in
+    let g3 = make [p3] in
+    let g4 = make [p1; p2] in
+    let g5 = make [p2; p3] in
+    let g6 = make [p3; p1] in
+    
+    (* Answers *)
+    let r1 = C.make (0.5,1.) 3.5 (halfway_color blue red) in
+    let r2 = C.make (2.,3.) 4.5 (halfway_color red green) in
+    let r3 = C.make (1.5,2.) 4. (halfway_color blue green) in
+    
+    assert(equals (sexual_reproduction 0. g1 g2) (make [r1]));
+    assert(equals (sexual_reproduction 0. g2 g3) (make [r2]));
+    assert(equals (sexual_reproduction 0. g1 g3) (make [r3]));
+    assert(equals (sexual_reproduction 0. g4 g5) (make [r1; r2]));
+    assert(equals (sexual_reproduction 0. g4 g6) (make [r3; r1]));
+    assert(equals (sexual_reproduction 0. g5 g6) (make [r2; r3]))
+
+  let run_tests () =
+    test_sexual_reproduction ();
+    () 
+end
+
+module MakePolygonGuess (P : POLYGON) : GUESS with type guess=P.polygon array =
+struct
+  type lego = P.polygon
+  type guess = lego array
+  
+  let fresh_v width height m v = Array.init ~f:(fun _ -> P.fresh width height v) m
+  
+  (* Number of vertices is always 1 *)
+  let fresh w h m = fresh_v w h m 1
+  
+  let legos g = g
+
+  let make lst = Array.of_list lst
+
+  let matrix_of_guess _ = Array.make_matrix ~dimx:5 ~dimy:12 black
 
   let cost _ _ = failwith "TODO"
 
@@ -96,4 +156,8 @@ struct
 end
 
 (* Applies the functor to make a Guess using our implementation of Polygon *)
-module Guess : GUESS = MakeGuess(Polygon)
+(* module Guess : GUESS = MakePolygonGuess(Polygon) *)
+
+(* Applies the functor to make a Guess using our implementation of Circle *)
+module Guess : GUESS = MakeCircleGuess(Circle)
+
